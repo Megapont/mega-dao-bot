2a16f361a8fd741657be7b77db17da688b71ad19
Zishan <amanzishan.az@gmail.com>
2024-02-19 20:26:25 +0530

    feat: build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 // Require the necessary discord.js classes
+
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -9,10 +13,9 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+client.once(Events.ClientReady, (readyClient: Client) => {
+	readyClient && console.log(`Ready! Logged in as ${readyClient.user!.tag}`);
 });
 
 // Log in to Discord with your client's token
-client.login(
-  process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
